0d1b51d27e261a9904de737a4ee64d11d6688983
zahra ellioun <zizi.ell75@gmail.com>
2025-08-19 13:56:55 +0330

    write useLocalStorage as a custom hook for give key and default value and set it in local storage or read from local storage and return a value and setValue

--- src/components/use-darkmode/index.jsx
+++ src/components/use-darkmode/index.jsx
@@ -1,15 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const LightDarkMode = () => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(() => {
+    const themeLocalStorage = localStorage.getItem("theme");
+    return themeLocalStorage;
+  });
+
+  const handleDarkMode = () => {
+    setTheme(theme === "dark" ? "light " : "dark");
+  };
+  console.log(theme);
+
+  useEffect(() => {
+    try {
+      localStorage.setItem("theme", theme);
+    } catch (e) {
+      console.log(e.massage);
+    }
+  }, [theme]);
+
   return (
     <div
-      className="bg-white text-black dark:bg-black dark:text-white"
-      data-theme={theme}
+      data-mode={theme}
+      className=" bg-white text-black dark:bg-black dark:text-white"
     >
       <div className="w-full h-screen">
         <p className="text-5xl"> HELLO WORLD!</p>
-        <button className="px-4 py-2 bg-black dark:bg-white text-white dark:text-black">
+        <button
+          onClick={handleDarkMode}
+          className="px-4 py-2 bg-black dark:bg-white text-white dark:text-black"
+        >
           change theme
         </button>
       </div>
